6000afe60032ea9d5901565c2f1b6ebcdab7aac1
BersenevaAV <Berseneva.A.V@yandex.ru>
2025-01-23 00:12:38 +0300

    stats style

--- stats/stats-client/src/test/java/ru/practicum/StatsClientAppTest.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum;
-
-import junit.framework.TestCase;
-
-public class StatsClientAppTest extends TestCase {
-    public StatsClientAppTest(String testName) {
-        super(testName);
-    }
-}
--- stats/stats-server/src/test/java/ru/practicum/StatsServerAppTest.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum;
-
-import junit.framework.TestCase;
-
-public class StatsServerAppTest extends TestCase {
-    public StatsServerAppTest(String testName) {
-        super(testName);
-    }
-}
