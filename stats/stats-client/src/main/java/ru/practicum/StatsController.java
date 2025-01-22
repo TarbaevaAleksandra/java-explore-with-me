93f1910bc36d53229114fc2ab0f8880804db633b
BersenevaAV <Berseneva.A.V@yandex.ru>
2025-01-23 00:17:09 +0300

    stats style

--- stats/stats-client/src/main/java/ru/practicum/StatsController.java
+++ stats/stats-client/src/main/java/ru/practicum/StatsController.java
@@ -3,12 +3,10 @@ package ru.practicum;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.EndpointHitDto;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
