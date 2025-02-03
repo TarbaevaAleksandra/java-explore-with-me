e81bb65391dd0286d69760ca4a20ca8294e7baa7
BersenevaAV <Berseneva.A.V@yandex.ru>
2025-02-03 15:33:59 +0300

    Замечания

--- ewm-service/src/main/java/ru/practicum/compilation/CompilationRepository.java
+++ ewm-service/src/main/java/ru/practicum/compilation/CompilationRepository.java
@@ -2,11 +2,8 @@ package ru.practicum.compilation;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-@Repository
 public interface CompilationRepository extends JpaRepository<Compilation, Long> {
     List<Compilation> findAllByPinned(Boolean pinned, Pageable pageable);
 }
--- ewm-service/src/main/java/ru/practicum/event/EventRepository.java
+++ ewm-service/src/main/java/ru/practicum/event/EventRepository.java
@@ -5,17 +5,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import ru.practicum.event.model.Event;
 import ru.practicum.event.model.State;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
 
-    @Query(value = "select * from events " +
-            "where initiator = ?1 ", nativeQuery = true)
+    @Query(value = "select * from events where initiator = ?1 ", nativeQuery = true)
     Page<Event> findEventsByUserId(Long userId, Pageable pageable);
 
     List<Event> findAllByIdIn(List<Long> ids);
