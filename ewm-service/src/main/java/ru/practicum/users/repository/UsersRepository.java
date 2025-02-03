e7c92cb13911ffcb1007faa1a713cad6ca6f75a2
BersenevaAV <Berseneva.A.V@yandex.ru>
2025-02-03 15:55:08 +0300

    Стиль

--- ewm-service/src/main/java/ru/practicum/users/repository/UsersRepository.java
+++ ewm-service/src/main/java/ru/practicum/users/repository/UsersRepository.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 public interface UsersRepository extends JpaRepository<User, Long> {
     @Query(value = """
-                      select * from users 
-                       where (id in (?1) or (coalesce(?1) = 0)) 
+                      select * from users
+                       where (id in (?1) or (coalesce(?1) = 0))
                        limit ?3 offset ?2 """, nativeQuery = true)
     List<User> getUsers(List<Long> ids, Integer from, Integer size);
 }
